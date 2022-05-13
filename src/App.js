391d7a12234f4fd72716dd4cf239dc3f078d6fc7
rafaelarissa <rafaela.larissalj@gmail.com>
2022-05-12 23:04:17 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
