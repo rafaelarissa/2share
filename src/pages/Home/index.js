8c9e94c7e5b67fd4d7efd74e394631aee94789c4
rafaelarissa <rafaela.larissalj@gmail.com>
2022-05-19 21:44:18 -0300

    feat: git commit set integration with spotify API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,32 +2,142 @@ import { Container } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import dotenv from "dotenv";
+import Listbox from "../../components/ListBox";
+import Detail from "../../components/Detail";
+import Dropdown from "../../components/Dropdown";
 dotenv.config();
 
 export default function Home() {
   const [token, setToken] = useState("");
-  var spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
-  var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+  const [genres, setGenres] = useState({
+    selectedGenre: "",
+    listOfGenresFromAPI: [],
+  });
+  const [playlist, setPlaylist] = useState({
+    selectedPlaylist: "",
+    listOfPlaylistFromAPI: [],
+  });
+  const [tracks, setTracks] = useState({
+    selectedTrack: "",
+    listOfTracksFromAPI: [],
+  });
+  const [trackDetail, setTrackDetail] = useState(null);
+
+  var spotify_client_id = "e529efd86cf04fcaa28f718c2d5cdcba";
+  var spotify_client_secret = "ee12f3b8e1c74efabdef9c0e1fc12ab5";
+  console.log(spotify_client_id);
+  console.log(spotify_client_secret);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://accounts.spotify.com/authorize?response_type=code&client_id=e5b97cbfa717430682bea67a961be3b8&scope=user-read-private%20user-read-email&redirect_uri=http%3A%2F%2Flocalhost%3A8888%2Fnew-playlist&state=yM4kPxQyelGY4G4o%22"
-        // {
-        // headers: {
-        //   "Content-Type": "application/x-www-form-urlencoded",
-        //   Authorization:
-        //     "Basic " + btoa(spotify_client_id + ":" + spotify_client_secret),
-        // },
-        // data: "grant_type=client_credentials",
-        // method: "POST",
-        // }
-      )
-      .then((tokenResponse) => {
-        console.log(tokenResponse);
-        setToken(tokenResponse.data.access_token);
-      })
-      .catch((error) => console.log(error));
-  }, []);
-  return <Container>teste alo som</Container>;
+    axios("https://accounts.spotify.com/api/token", {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " + btoa(spotify_client_id + ":" + spotify_client_secret),
+      },
+      data: "grant_type=client_credentials",
+      method: "POST",
+    }).then((tokenResponse) => {
+      setToken(tokenResponse.data.access_token);
+      console.log(tokenResponse.data.access_token);
+
+      axios("https://api.spotify.com/v1/browse/categories?locale=sv_US", {
+        method: "GET",
+        headers: { Authorization: "Bearer " + tokenResponse.data.access_token },
+      }).then((genreResponse) => {
+        setGenres({
+          selectedGenre: genres.selectedGenre,
+          listOfGenresFromAPI: genreResponse.data.categories.items,
+        });
+      });
+    });
+  }, [genres.selectedGenre, spotify_client_id, spotify_client_secret]);
+
+  const genreChanged = (val) => {
+    setGenres({
+      selectedGenre: val,
+      listOfGenresFromAPI: genres.listOfGenresFromAPI,
+    });
+
+    axios(
+      `https://api.spotify.com/v1/browse/categories/${val}/playlists?limit=10`,
+      {
+        method: "GET",
+        headers: { Authorization: "Bearer " + token },
+      }
+    ).then((playlistResponse) => {
+      setPlaylist({
+        selectedPlaylist: playlist.selectedPlaylist,
+        listOfPlaylistFromAPI: playlistResponse.data.playlists.items,
+      });
+    });
+
+    console.log(val);
+  };
+
+  const playlistChanged = (val) => {
+    console.log(val);
+    setPlaylist({
+      selectedPlaylist: val,
+      listOfPlaylistFromAPI: playlist.listOfPlaylistFromAPI,
+    });
+  };
+
+  const buttonClicked = (e) => {
+    e.preventDefault();
+
+    axios(
+      `https://api.spotify.com/v1/playlists/${playlist.selectedPlaylist}/tracks?limit=10`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      }
+    ).then((tracksResponse) => {
+      setTracks({
+        selectedTrack: tracks.selectedTrack,
+        listOfTracksFromAPI: tracksResponse.data.items,
+      });
+    });
+  };
+
+  const listboxClicked = (val) => {
+    const currentTracks = [...tracks.listOfTracksFromAPI];
+
+    const trackInfo = currentTracks.filter((t) => t.track.id === val);
+
+    setTrackDetail(trackInfo[0].track);
+  };
+
+  return (
+    <Container>
+      <form onSubmit={buttonClicked}>
+        <Dropdown
+          label="Genre :"
+          options={genres.listOfGenresFromAPI}
+          selectedValue={genres.selectedGenre}
+          changed={genreChanged}
+        />
+        <Dropdown
+          label="Playlist :"
+          options={playlist.listOfPlaylistFromAPI}
+          selectedValue={playlist.selectedPlaylist}
+          changed={playlistChanged}
+        />
+        <div className="col-sm-6 row form-group px-0">
+          <button type="submit" className="btn btn-success col-sm-12">
+            Search
+          </button>
+        </div>
+        <div className="row">
+          <Listbox
+            items={tracks.listOfTracksFromAPI}
+            clicked={listboxClicked}
+          />
+          {trackDetail && <Detail {...trackDetail} />}
+        </div>
+      </form>
+    </Container>
+  );
 }
