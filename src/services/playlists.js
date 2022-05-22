b65c2ef849965cf67594fdd7ba4427cd80163d5b
rafaelarissa <rafaela.larissalj@gmail.com>
2022-05-22 03:35:24 -0300

    feat: added list service

--- src/services/playlists.js
+++ src/services/playlists.js
@@ -1,7 +1,11 @@
 import api from "./api";
 
 export async function create() {
-  const response = await api.get("/playlists");
+  const response = await api.post("/playlists");
+  return response.data;
+}
 
+export async function list() {
+  const response = await api.get("/playlists");
   return response.data;
 }
