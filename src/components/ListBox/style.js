5e718e2fd4bf49655dcfb6a8f57e8d22dce9ca91
rafaelarissa <rafaela.larissalj@gmail.com>
2022-05-22 03:04:26 -0300

    feat: set responsive layout to home page

--- src/components/ListBox/style.js
+++ src/components/ListBox/style.js
@@ -2,9 +2,15 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: grid;
+
   grid-template-columns: repeat(6, 1fr);
   gap: 20px;
   align-items: start;
+
+  @media (max-width: 800px) {
+    margin-left: 50px;
+    grid-template-columns: repeat(3, 1fr);
+  }
 `;
 
 const TrackContainer = styled.div`
@@ -25,6 +31,21 @@ const TrackContainer = styled.div`
 
   .artist-name {
     font-size: 14px;
+  }
+
+  @media (max-width: 800px) {
+    img {
+      width: 95%;
+      height: 95%;
+    }
+
+    .track-name {
+      font-size: 13px;
+    }
+
+    .artist-name {
+      font-size: 12px;
+    }
   }
 `;
 
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -16,7 +16,6 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import { AddBox, Home, Search } from "@mui/icons-material";
 
