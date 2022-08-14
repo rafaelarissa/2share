c10443e894b566e63a8b4f88e1a9b3b15513ec7c
rafaela justino <rafaela.larissalj@gmail.com>
2022-08-14 15:36:49 -0300

    feat: preparing for deploy

--- src/components/AddSongsToPlaylist/index.js
+++ src/components/AddSongsToPlaylist/index.js
@@ -1,5 +1,5 @@
 function AddSongsToPlaylist() {
-  alert("cliquei pra adicionar músicas à playlist");
+  alert("Adicionando novas músicas à playlist!");
   //adiciono aqui e mando pra playlist um array com as informações de músicas
 }
 
